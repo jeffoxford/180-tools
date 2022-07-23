7300e6efb23cffe1b5618740edfdb329708e73e5
gentjanlikaj <74827070+gentjanlikaj@users.noreply.github.com>
2022-07-23 09:46:41 +0200

    Update 08_Relevant_Page_Finder.py

--- streamall/pages/08_Relevant_Page_Finder.py
+++ streamall/pages/08_Relevant_Page_Finder.py
@@ -6,11 +6,11 @@ import pandas as pd
 
 st.title("Relevant Page Finder")
 
-domain =st.text_input('Single Domain')
-urls =st.text_area(label='Enter Keywords',placeholder='Enter Keywords (1 per line)')
+domain =st.text_input('Website Domain')
+urls =st.text_area(label='Enter Keywords (max 100)',placeholder='Enter Keywords (max 100)')
 lines = urls.split("\n")
 
-# to insert the api key for serp  like this >>>>>>>>>>> st.secrets['api_key3'] int he strwamlit secrets
+# to insert the api key for serp  like this >>>>>>>>>>> st.secrets['api_key3'] int he streamlit secrets
 def get_data(keyword,domain): 
     query = f'"{keyword}" site:{domain}'
     params = {
@@ -27,6 +27,7 @@ def get_data(keyword,domain):
 result_data = []
 for keyword in lines :
     r = get_data(keyword,domain)
+    total_results = r['search_information']['total_results']
     try:
         l1 = r['organic_results'][0]['link']
     except:
@@ -45,6 +46,7 @@ for keyword in lines :
             '#1 URL' : l1,
             '#2 URL' : l2,
             '#3 URL' : l2,
+            'total_results': total_results
         })
     except:
         result_data.append({
@@ -52,6 +54,7 @@ for keyword in lines :
             '#1 URL' : '',
             '#2 URL' : '',
             '#3 URL' : '',
+            'total_results': total_results
         })
 
 if st.button('Start Process The Keyword'):
